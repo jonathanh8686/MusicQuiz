50969ca939b2192d5d32b3f37a356ad1055d80d8
jaspaaaaaa <jasper20020202@gmail.com>
2020-11-27 12:59:17 -0800

    Testing song guesses

--- server/server.js
+++ server/server.js
@@ -18,6 +18,75 @@ function validateURL(url) {
   );
 }
 
+// -------------- code for testing song guesses ------------------
+function levenstein(purported, actual){
+  // ignoring spaces and punctuations
+  const isAlphaNumeric = ch => ch.match(/^[a-z0-9]+$/i) !== null;
+  
+  const processPurported = purported.split("")
+    .filter(char => isAlphaNumeric(char))
+    .map(char => char.toLowerCase())
+    .sort(); // x string
+  const processActual = actual.split("")
+    .filter(char => isAlphaNumeric(char))
+    .map(char => char.toLowerCase())
+    .sort(); // y string
+
+  // initialize 2D array of dimension len(x) by len(y) [hori by vert]
+  var dp = new Array(processActual.length + 1).fill(0).map(
+    () => new Array(processPurported.length + 1))
+
+  // j is column, i is row
+  for (var j = 0; j < processPurported.length + 1; j ++){
+    dp[0][j] = j;
+  }
+  for (var i = 0; i < processActual.length + 1; i ++){
+    dp[i][0] = i;
+  }
+
+  // if we have computed the first outer "fringe" of edit distances
+  // there are processActual.length - 1 more rows to process
+
+  for (var i = 1; i < processActual.length + 1; i ++){
+    for (var j = 1; j < processPurported.length + 1; j ++){
+      const matchCost = (processPurported[j - 1] === processActual[i - 1]) ? 0 : 1;
+      dp[i][j] = Math.min(
+        dp[i-1][j-1] + matchCost, // matching last char of x
+        dp[i-1][j] + 1,           // delete last char of x
+        dp[i][j-1] + 1,           // insert to the end of x
+      )
+    }
+  }
+
+  const minDistance = dp[processActual.length][processPurported.length];
+  const tolerance = Math.floor(processActual.length / 2);
+  // const resultString = minDistance <= tolerance ? "Accepted" : "Declined"
+  // console.log(purported.concat(" vs. ").concat(actual));
+  // console.log("Min Edit Distance Ignoring Order = ".concat(minDistance));
+  // console.log("Tolerance = ".concat(tolerance));
+  // console.log("The purported answer is ".concat(resultString).concat("\n\n"));
+  //return minDistance;
+  return minDistance <= tolerance;
+}
+
+function removeParenExp(str){
+  const parenRegex = /\(([^)]+)\)/;
+  const parenstr = parenRegex.exec(str);
+  if (parenstr) return str.replace(parenstr[0], "");
+  else return str;
+}
+
+// ! Use this function to test a purported answer with the actual song title !
+function testSongGuess(purported, actual){
+  const processActual = removeParenExp(actual);
+  // console.log(purported.concat(" vs. ").concat(actual))
+  // console.log(levenstein(purported, actual) || levenstein(purported, processActual))
+  // console.log("\n");
+  return levenstein(purported, actual) || levenstein(purported, processActual);
+}
+
+// ------------------------------------------------------------------
+
 const http = require("http");
 var request = require("request");
 const express = require("express");
@@ -67,12 +136,14 @@ MongoClient.connect(process.env.CONNECTIONSTRING, {
         for (var i = 0; i < results.length; i++) {
           codes.push(results[i]["roomid"]);
         }
+        // sends the room codes on a get request with /room-codes
         res.json(codes);
       })
       .catch((error) => console.error(error));
   });
 
   app.post("/add-room", jsonParser, (req, res) => {
+    // should also display some sort of an error message in browser
     if (!validateURL(req.body["spotify_url"])) return;
 
     db.collection("room-info")
@@ -89,6 +160,7 @@ MongoClient.connect(process.env.CONNECTIONSTRING, {
           roomid = createid.getRandID();
         }
 
+        // initialize an unstarted game
         let roominfo = {
           spotify_url: req.body["spotify_url"],
           started: false,
@@ -96,6 +168,7 @@ MongoClient.connect(process.env.CONNECTIONSTRING, {
           roomid: roomid,
         };
 
+        // add the new game to the database
         db.collection("room-info")
           .insertOne(roominfo)
           .then((result) => {
@@ -142,10 +215,10 @@ MongoClient.connect(process.env.CONNECTIONSTRING, {
               { roomid: data["room"] },
               {
                 $set: {
-                  roomid: room["roomid"],
+                  // roomid: room["roomid"],
                   players: room["players"].concat(data["user"]),
-                  spotify_url: room["spotify_url"],
-                  started: room["started"],
+                  // spotify_url: room["spotify_url"],
+                  // started: room["started"],
                 },
               }
             )
@@ -296,6 +369,10 @@ MongoClient.connect(process.env.CONNECTIONSTRING, {
       // check if it's the right answer
 
       // check if it's close
+      // implement this code once we get the spotify api going
+      // if (testSongGuess(data["purported"], data["actual"])){ // maybe the data should also include a time measurement
+      //   // add score (depending on time?) for the user
+      // }
 
       // if not then it's a normal message
       io.sockets.in(data["room"]).emit("chat_events", {
@@ -327,7 +404,7 @@ MongoClient.connect(process.env.CONNECTIONSTRING, {
           var list_id = urlparts[urlparts.length - 1];
 
           var options = {
-            url: room_setting["spotify_url"].includes("playlist")
+            url: room_setting["spotify_url"].includes("playlist") // it's either a playlist or an album
               ? `https://api.spotify.com/v1/playlists/${list_id}/tracks`
               : `https://api.spotify.com/v1/albums/${list_id}/tracks`,
             headers: {
